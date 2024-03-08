ab7ed225d2dd6fdf766936cad14f2a1b83edea93
Genadi94 <redfender2@gmail.com>
2024-03-08 21:14:24 +0200

    update08032024

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -4,6 +4,81 @@ import bcryptjs from 'bcryptjs';
 import jsonwebtoken from 'jsonwebtoken';
 import Account from '../models/accountModel.js';
 
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *      User:
+ *          type: object
+ *          required: 
+ *              - firstName
+ *              - lastName
+ *              - email
+ *              - password
+ *          properties:
+ *              id:
+ *                  type: integer
+ *                  description: The auto-generated id of the user
+ *              firstName:
+ *                  type: string
+ *                  description: The first name of the user
+ *              lastName:
+ *                  type: string
+ *                  description: The last name of the user
+ *              email:
+ *                  type: string
+ *                  description: The user email address
+ *              password:
+ *                  type: string 
+ *                  description: The user crypt password
+ *              verificationCode:
+ *                  type: integer 
+ *                  description: Generated code for account validation
+ *              isVerified:
+ *                  type: boolean
+ *                  description: Get default value of false untill user validation  
+ *          example:
+ *              id: 1
+ *              firstName: Genadi
+ *              lastName: Astafev
+ *              email: genadi123@mail.com
+ *              password: password
+ *              verificationCode: 1234
+ *              isVerified: false
+ *             
+ */
+
+/**
+ * @swagger
+ * tags:
+ *  name: Accounts
+ *  description: The accounts managing API
+ */
+
+
+
+
+/** 
+ * @swagger
+ * /api/account/signup:
+ *  post:
+ *      summary: Create new user account
+ *      tags: [Accounts]
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      $ref: '#/components/schemas/User'
+ *      responses:
+ *          200:
+ *              description: The user account was successfully created
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/User'
+*/
+
 router.post('/signup',(req,res) => {
    
     const {firstName,lastName,email,password} = req.body;
@@ -46,17 +121,72 @@ router.post('/signup',(req,res) => {
     })
 });
 
-router.get('/users', (req,res) => {
-    Account.findAll()
-    .then(results =>{
+/**
+ * @swagger
+ * /api/account/users:
+ *  get:
+ *      summary: Return the list of all users
+ *      tags: [Accounts]
+ *      responses: 
+ *          200:
+ *              description: The list of the users
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: array
+ *                          items: 
+ *                              $ref: '#/components/schemas/User'
+ */
+router.get("/users", (req,res) => {
+  Account.findAll()
+    .then((results) => {
       return res.status(200).json({
         message: results
+      })
+    })
+    .catch((error) => {
+      return res.status(500).json({
+        message: error,
+      });
+    });
+});
+/**
+ * @swagger
+ * /api/account/deleteAccount/{id}:
+ *  delete:
+ *      summary: Delete an account by id
+ *      tags: [Accounts]
+ *      parameters:
+ *          - in: path
+ *            name: id
+ *            schema:
+ *              type: integer
+ *            required: true
+ *            description: The user id
+ *      responses:
+ *          200:
+ *              description: The user account was deleted
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/User'
+ */
+
+router.delete('/deleteAccount/:id', (req,res) => {
+    const userId = req.params.id;
+    Account.findByPk(userId)
+    .then(results => {
+      return results.destroy()
+      .then(account => {
+        return res.status(200).json({
+          message:account
+        })
       })
     })
     .catch(error => {
       return res.status(500).json({
         message: error
-    })
+      })
     })
 })
 
@@ -64,7 +194,6 @@ router.post('/verify',(req,res) => {});
 
 router.post('/login', (req,res) => {});
 
-router.get('/getAccounts',(req,res) => {});
 
 
 
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from 'dotenv';
 import swaggerJSDoc from "swagger-jsdoc";
-import  SwaggerUi from "swagger-ui-express";
+import SwaggerUi  from "swagger-ui-express";
 import accountController from './controllers/accountController.js'
 import database from './services/database.js';
 
@@ -14,15 +14,20 @@ app.use(express.json());
 
 
 const swaggerOptions = {
-    definition: {
+    definition:  {
         openapi: '3.0.0',
         info: {
-            title: 'Findscooter API Documentation',
-            description: 'This is my first Swagger documentation'
+            title: 'FindScooter API Documentation',
+            version: '1.0.0',
+            description: 'This is my first swagger documentation'
         },
     },
-    apis: ['']
+    apis: ['./controllers/accountController.js']
 }
+
+
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+app.use('/api-docs', SwaggerUi.serve,SwaggerUi.setup(swaggerDocs));
 
 
 
